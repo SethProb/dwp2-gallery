a1cb72c2e85acc5541fa6e58d1a3be6466962460
SethProb <seth@probabilityworks.com>
2026-03-30 12:07:10 -0500

    Update Google Sheets URL construction in handler

--- api/sheet.js
+++ api/sheet.js
@@ -1,10 +1,13 @@
 export default async function handler(req, res) {
-  const url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTjgaU9R0HkOYxMojb3CAQ0E0-wAaLMHejbEB1aPKG_xw-chZwJJBv9_inZUSVc2J_clRTXDASNLKlf/pub?gid=0&single=true&output=csv';
+  const sheetId = '1rJoNaO2I2anVkQmsuGnS6xMMkIcIF2K5NQ9iaH5W5NY';
+  const url = `https://docs.google.com/spreadsheets/d/${sheetId}/export?format=csv&gid=0`;
   try {
     const response = await fetch(url);
+    if (!response.ok) throw new Error('Google returned ' + response.status);
     const text = await response.text();
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Content-Type', 'text/plain');
+    res.setHeader('Cache-Control', 'no-store');
     res.status(200).send(text);
   } catch (err) {
     res.status(500).send('Error fetching sheet: ' + err.message);
